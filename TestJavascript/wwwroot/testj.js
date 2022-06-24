f75e128c37e120d333844844efc172a85b8fe558
Fadi Nassri <fadinassri@gmail.com>
2022-06-24 13:17:31 -0300

    TestJavaScriptForJenkins

--- TestJavascript/wwwroot/testj.js
+++ TestJavascript/wwwroot/testj.js
@@ -1,2 +1,8 @@
 ﻿console.log("Disable Organization Folder");
 console.log("******************************************************************************");
+
+var divsToHide = document.getElementsByClassName("jenkins_branch_OrganizationFolder"); //divsToHide is an array
+for (var i = 0; i < divsToHide.length; i++) {
+    divsToHide[i].style.visibility = "hidden"; // or
+    divsToHide[i].style.display = "none"; // depending on what you're doing
+}
